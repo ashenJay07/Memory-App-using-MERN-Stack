9b5fe70be8a5cdaa1cd96b02825e31c0c889cf09
Ashen <ashenj.official@gmail.com>
2023-02-14 19:10:28 +0530

    implementing database connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
+// using dotenv file
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+// using an express
+const app = express();
+
+// implementing default middlewares that provided by express
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const MongoDB_URI = process.env.MongoDB_URI;
+const PORT = process.env.PORT || 5001;
+
+mongoose.set('strictQuery', true); // must use before implementing db connection
+// Mongoose will ensure that only the fields that are specified in your Schema will be saved in the database
+
+// creating database connection usign mongoose
+mongoose
+  .connect(MongoDB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server runnig on PORT ${PORT}...`))
+  )
+  .catch((error) => console.log(error));
