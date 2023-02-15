d57fc530764814d464c0d83d457819d25b932540
Ashen <ashenj.official@gmail.com>
2023-02-15 17:45:23 +0530

    completed upto JSX structure - 28:50 part

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+// importing components
+import Form from './components/form/Form';
+import Posts from './components/posts/Posts';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOP34-CDeSEO36VW3uGFqODfwqTfgulJbnCs6qiAvs&s"
+          alt="memories"
+          height={400}
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Post from './post/Post';
+
+const Posts = () => {
+  return (
+    <>
+      <h1>POSTS</h1>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
